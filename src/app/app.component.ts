dbd3fb68636048485e99ae5a4ca0ad521f18112a
Steven Sullivan <stevensullivan256@gmail.com>
2017-08-25 09:57:59 -0700

    Passed balance param to categories page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' },
+      { title: 'Home', component: HomePage },
       { title: 'Add Expense', component: 'AddExpensePage' },
       { title: 'Edit Categories', component: 'CategoriesPage' },
       { title: 'Balances', component: 'BalancesPage' },
